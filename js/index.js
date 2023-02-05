0861c8e73a428048bc7561d5e29942ad45fc9dd9
Ilya Yalymov <chezeee@gmail.com>
2023-02-05 22:53:57 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// define classes here
+
+// Sample usage - do not modify
+const galaxy = new Android("Galaxy", 400);
+galaxy.getDescription(); // "The Galaxy costs 400"
+galaxy.getVersion(); // 12 (always for android phones)
+
+const iphone = new iOS("iPhone", 500);
+iphone.getDescription(); // "The iPhone costs 500"
+iphone.getVersion(); // 15 (always for iOS phones)
